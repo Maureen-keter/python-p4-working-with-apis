a63145bce0c31c50891ede0d533aaa393d07b064
maureen <maureen.chelangat@student.moringaschool.com>
2024-01-12 08:46:01 +0300

    completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,22 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+   
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+            programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+    return programs_list
+
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
